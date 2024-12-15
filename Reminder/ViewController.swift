767761dcb82b4f0418a06ebe6db9bbf5cedd8aa4
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-12-15 17:06:52 -0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Lucas Rosa  on 15/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
